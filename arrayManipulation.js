0b92870373092c3e2834590cbf668e035dbb1a87
BenGates <bengates28bgt@gmail.com>
2024-05-19 17:10:41 +0000

    Added a Function

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,4 +11,19 @@ function processArray(arr) {
 const inputArray = [1, 2, 3, 4, 5];
     const resultArray = processArray(inputArray);
     console.log(resultArray);
+
+    function formatArrayStrings(strArr, numArr) {
+      return strArr.map((str, index) => {
+        if (numArr[index] % 2 === 0) {
+          return str.toUpperCase();
+        } else {
+          return str.toLowerCase();
+        }
+      });
+    }
     
+    const inputStrings = ['Foo', 'BAR', 'baz', 'qux'];
+    const inputNumbers = [2, 1, 3, 4];
+    const formattedStrings = formatArrayStrings(inputStrings, inputNumbers);
+    console.log(formattedStrings);
+
